de4596da467cb393bb64d083cb69aff2da8885ec
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2023-03-01 16:33:46 -0800

    checked image loading and added infinite scroll function

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const loader = document.getElementById('loader')
 
 
 let photosArray = []
+let imgloadeddd = 0
+let allImgs = 0
+let ready = false
 
 
 
@@ -11,6 +14,17 @@ const count = 30;
 const apiKey = 'gfmF9WwJhDBr0msBkqUqzz7bZAwWOlp4ggRAr6wuW-I'
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
 
+
+// Check if all images were loaded
+function imgLoaded(){
+    console.log('image loaded')
+    imgloadeddd++
+    if(imgloadeddd === allImgs){
+        ready = true 
+        loader.hidden = true
+        console.log(ready)
+    }
+}
 
 
 /// Fetch Photos from unsplash API
@@ -24,42 +38,54 @@ async function getPhotos(){
     }
 }
 
-
+//  Helper function to set Attributes on Elements
 function setAttributes(element, attributes){
-    for (const key in attributes){
+    for(const key in attributes){
         element.setAttribute(key, attributes[key]);
     }
 }
+
 /// Create elements for links & photo, then add to DOM
 function displayPhotos(){
-    console.log(photosArray)
+
+    imgloadeddd = 0
+    allImgs = photosArray.length
+    // Run function for each object in PhotosArray
     photosArray.forEach((photo) => {
 
+        // Create <a> to link to full photo
         const item = document.createElement('a');
-        // item.setAttribute('href',photo.links.html);
-        // item.setAttribute('target', '_blank')
         setAttributes(item, {
             href: photo.links.html,
             target: '_blank'
         })
 
+        // create <img> for photo
         const img = document.createElement('img');
-        // img.setAttribute('src', photo.urls.regular)
-        // img.setAttribute('alt', photo.discription)
-        // img.setAttribute('title',photo.discription)
         setAttributes(img, {
             src: photo.urls.regular,
             alt: photo.discription,
             title: photo.discription
         })
+        
+        // Add eventlistner when each is finished loading
+        img.addEventListener('load', imgLoaded)
+
         //Add item and image into Image container 
         item.appendChild(img)
         imageContainer.appendChild(item)
     })
-
-
 }
 
 
+// Reload all imgs when user scroll almost to the end of the page
+window.addEventListener('scroll', () => {
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready)
+    getPhotos();
+})
 
+
+
+
+// On load
 getPhotos()
